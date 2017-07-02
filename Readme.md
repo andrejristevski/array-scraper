b8dff320f41e4a3b5f46d211c9f5231f8f68e157
andrejristevski <andrej.rist@gmail.com>
2017-07-02 08:33:24 +0200

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# array-scraper
+
+array-scraper is a module that let us download array of urls in sequential manner and returns success bodies and errors.
+
+### Example Usage
+
+let arrayScrapper = require('array-scraper');
+let options = {
+    timeout: 50, //timeout between http calls 
+    logtotal: false
+}
+
+arrayScrapper(urls, function (successBodies, errors) {
+
+} , ? options);
---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const http = require('http');
+const request = require('request');
+const merge = require('merge');
+
+let res = [];
+let errors = [];
+let totalCounter = 0;
+let config = {
+    timeout: 50,
+    logtotal: true
+}
+
+function callNext(urlFull, callback) {
+    new Promise(() => {
+        request(urlFull, function (error, response, body) {
+            if (error) {
+                callback(null, error);
+            } else {
+                callback(body);
+            }
+        });
+    });
+}
+
+function* iter(urls, callback, next) {
+    for (k in urls) {
+
+        yield callNext(urls[k], function (as, error) {
+            if (error) {
+                setTimeout(function () {
+                    errors.push(JSON.parse(error))
+                    if (config.logtotal) {
+                        console.log(`${++totalCounter} out of ${urls.length} downloaded`);
+                    }
+                    next();
+                }, config.timeout);
+            } else {
+                setTimeout(function () {
+                    res.push(JSON.parse(as));
+                    if (config.logtotal) {
+                        console.log(`${++totalCounter}out of ${urls.length} downloaded`);
+                    }
+                    next();
+                }, config.timeout);
+            }
+        });
+    }
+    callback(res, errors);
+}
+
+const downloadUrls = function (urls, callback, options) {
+    merge(config, options);
+    let gen = iter(urls, callback, function () {
+        gen.next();
+    });
+    gen.next();
+}
+
+module.exports = downloadUrls;
